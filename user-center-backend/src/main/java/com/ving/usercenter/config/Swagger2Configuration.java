f80c72badac7ffcc0d18f6715a4ca0f8faeb6709
MyVing <2960532192@qq.com>
2024-05-07 00:52:54 +0800

    自定义Swagger文档

--- user-center-backend/src/main/java/com/ving/usercenter/config/Swagger2Configuration.java
+++ user-center-backend/src/main/java/com/ving/usercenter/config/Swagger2Configuration.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,7 +25,8 @@ public class Swagger2Configuration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                // 这里需要将改为controller位置便于自动生成接口
+                .apis(RequestHandlerSelectors.basePackage("com.ving.usercenter.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
@@ -32,9 +34,10 @@ public class Swagger2Configuration {
     //基本信息的配置，信息会在api文档上显示
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
-                .title("zg测试的接口文档")
-                .description("xx相关接口的文档")
-                .termsOfServiceUrl("http://localhost:8080/hello")
+                .title("用户中心")
+                .description("接口文档")
+                .termsOfServiceUrl("https://github.com/ving")
+                .contact(new Contact("ving","https://github.com/ving","2960532192@qq.com"))
                 .version("1.0")
                 .build();
     }
